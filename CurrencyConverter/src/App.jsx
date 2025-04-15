d1630f0914baa400e458bcd895ed505d7ca4b0ad
sanjeet <skkarmasi420@gmail.com>
2025-04-15 21:31:09 +0530

    massage

--- CurrencyConverter/src/App.jsx
+++ CurrencyConverter/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import useCurrencyInfo from './hooks/useCurrencyInfo';
+import useCurrencyInfo from './Hooks/useCurrencyInfo';
 
 function App() {
   const [amount, setAmount] = useState(1);
